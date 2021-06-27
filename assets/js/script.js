b19ead981037a83e4fe974a44341e0abd1f948dc
Matilda Brantley <matildahbrantley@gmail.com>
2021-06-27 00:15:28 -0400

    basic layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-var resultTextEl = $('#result-text');
-var resultContentEl = $('#result-content');
-var searchFormEl = document.querySelector('#search-form');
+var resultText = $('#result-text');
+var resultContent = $('#result-content');
+var searchForm = document.querySelector('#search-form');
 const apiKey = 'a641bf9af84171a774e42ab0fb8b528b';
 
 //TODO: change to address
@@ -18,8 +18,8 @@ async function getCityCoordinates(city) {
     });
 };
 
-//get current data by coordinates
-async function getCurrentWeather(latitude, longitude) {
+//get One Call weather data by coordinates
+async function getWeather(latitude, longitude) {
     var weatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude +'&units=imperial&appid=' + apiKey;
     //returjn fetch Promise of weather data object
     return fetch(weatherUrl)
@@ -32,39 +32,66 @@ async function getCurrentWeather(latitude, longitude) {
     });
 };
 
-//get 5-day forecast data by coordinates
-async function getForecastWeather(latitude, longitude) {
-    var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude +'&units=imperial&appid=' + apiKey;
-    //returjn fetch Promise of weather data object
-    return fetch(weatherUrl)
-    .then(function (response) {
-        console.log(response);
-        return response.json();
-    })
-    .then(function (weatherData) {
-        return weatherData;
-    });
-};
-
 //fecth all necessary weather data, both current and 5-day forecast, and display
-async function getWeather(city){
+async function weather(city){
     //get city's coordinates
     var coords = await getCityCoordinates(city);
     //use coordinates to get current weather of city
-    var currentWeather = await getCurrentWeather(coords[0], coords[1]);
-    console.log(currentWeather);
-    var forecastWeather = await getForecastWeather(coords[0], coords[1]);
-    console.log(forecastWeather);
-    displayCurrentWeather(currentWeather); 
-    displayForecastWeather(forecastWeather);
+    var weather = await getWeather(coords[0], coords[1]);
+    //display weather
+    displayWeather(weather); 
 }
 
-function displayCurrentWeather(currentWeather){
+function displayWeather(weather){
+    currentWeather = weather.current;
+    var date = getDate(currentWeather.dt);
+    console.log(date);
 
+    forecastDays = weather.daily;
+    for (var d=1; d<6; d++) {
+        date = getDate(forecastDays[d].dt);
+        console.log(date);
+    }
 }
 
-function displayForecastWeather(forecastWeather){
-    
+
+weather("Atlanta");
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getDate(unixTime){
+    return moment.unix(unixTime).format("MM/DD/YYYY")
 }
 
-getWeather("Atlanta");
+
+// function display5DayPer3HourForecast(forecastWeather){
+//     forecastDays = forecastWeather.list;
+//     for (var i=0; i<5*8; i++) {
+//         var date = moment.unix(forecastDays[d].dt).format("MM/DD/YYYY");
+//         console.log(date);
+//     }
+// }
+
+// //get 5-day per 3 hour forecast data by coordinates
+// async function get5DayPer3HourForecast(latitude, longitude) {
+//     var weatherUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude +'&units=imperial&appid=' + apiKey;
+//     //returjn fetch Promise of weather data object
+//     return fetch(weatherUrl)
+//     .then(function (response) {
+//         console.log(response);
+//         return response.json();
+//     })
+//     .then(function (weatherData) {
+//         return weatherData;
+//     });
+// };
